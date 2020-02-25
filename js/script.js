08bb68d8a7ad9ef0db5cd2bc75c490cb0c893254
evasquezASC <ernievazk7979@gmail.com>
2020-02-25 23:34:41 +0000

    Added changes to JavaScript

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let playList = [song1, song2, song3, song4, song5];
+
+playList.forEach(function(song){
+    $(".songNames")
+})
+
+let song1 = {
+    name: "What's Up Danger",
+    artist: "BlackWay_BlackCaviar",
+    length: "3:43",
+    image: "https://imgur.com/mypic.jpg"
+}
+
+// <img src=
+//let playList = [songArtists, songName, songLength, image, link];
+
