bbb1b6f04543a32108d8b4e85d03731fa21ddc21
evasquezASC <ernievazk7979@gmail.com>
2020-03-03 23:32:32 +0000

    Edited the JavaScript and HTML

--- js/script.js
+++ js/script.js
@@ -1,16 +1,45 @@
-let playList = [song1, song2, song3, song4, song5];
-
-playList.forEach(function(song){
-    $(".songNames")
-})
-
 let song1 = {
     name: "What's Up Danger",
-    artist: "BlackWay_BlackCaviar",
+    artist: "BlackWay&BlackCaviar",
     length: "3:43",
-    image: "https://imgur.com/mypic.jpg"
+    image: "https://i.imgur.com/faz3RrB.jpg"
 }
 
-// <img src=
+let song2 = {
+    name: "What's Up Danger",
+    artist: "BlackWay&BlackCaviar",
+    length: "3:43",
+    image: "https://i.imgur.com/faz3RrB.jpg"
+}
+
+let song3 = {
+    name: "What's Up Danger",
+    artist: "BlackWay&BlackCaviar",
+    length: "3:43",
+    image: "https://i.imgur.com/faz3RrB.jpg"
+}
+
+let song4 = {
+    name: "What's Up Danger",
+    artist: "BlackWay&BlackCaviar",
+    length: "3:43",
+    image: "https://i.imgur.com/faz3RrB.jpg"
+}
+
+let song5 = {
+    name: "What's Up Danger",
+    artist: "BlackWay&BlackCaviar",
+    length: "3:43",
+    image: "https://i.imgur.com/faz3RrB.jpg"
+}
+
+
+let playList = [song1, song2, song3, song4, song5];
+/*
+playList.forEach(function(song){
+
+})
+*/
+// 
 //let playList = [songArtists, songName, songLength, image, link];
 
